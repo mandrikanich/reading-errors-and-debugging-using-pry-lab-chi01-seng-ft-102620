aaeb1ee612e93fc4b872daac642d3e151838aced
Michael Andrikanich <mandrikanich@gmail.com>
2020-10-13 17:40:11 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,10 +1,8 @@
 require 'pry'
 
 def start_game(player1, player2)
-  player1 == Rocky
-  player2 == Bullwinkle
   binding.pry
-  "Hello #{player1} & #{playar2}"
+  "Hello #{player1} & #{player2}"
 end
 
 def play_game(player2)
