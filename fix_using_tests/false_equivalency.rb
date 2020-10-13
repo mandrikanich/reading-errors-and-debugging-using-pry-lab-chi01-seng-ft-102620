886dce8e13c3a73a5579453ce18b55770e055e83
Michael Andrikanich <mandrikanich@gmail.com>
2020-10-13 16:47:31 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -21,11 +21,12 @@ def prompt_user
 end
 
 def selection(num)
+  binding.pry
   if num = 1
-    #binding.pry
+    binding.pry
      "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
-    #binding.pry
+    binding.pry
      "HAM HAM HAM IN MY TUMMY"
   end
 end
